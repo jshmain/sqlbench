3c7e6bc051a50e46887377eabfa8c29ba4dacc90
jshmain@gmail.com <jeff123>
2016-06-21 06:15:44 -0700

    Initial version

--- dist.sh
+++ dist.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 # Create folder structure
-mkdir dist
-cp -r configs dist
-cp -r python dist
-cp -r impal
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <release version>"
+        exit 1
+fi
+
+export release_dir=$1
+mkdir $release_dir
+
+cp -r configs $release_dir
+cp -r python $release_dir
+cp -r suites $release_dir
+
+
+tar -cvf  $release_dir.tar $release_dir
+gzip $release_dir.tar
+rm -rf $release_dir
