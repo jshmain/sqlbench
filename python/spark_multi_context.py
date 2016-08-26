b57aadcb8f48b5be160f2dff9d54679de0bd3b51
Jeffrey Shmain <jshmain@gmail.com>
2016-08-26 14:05:33 -0700

    Create spark_multi_context.py

---
+++ python/spark_multi_context.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+import ConfigParser
+import time
+import os
+import subprocess
+
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+LOG = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        logging.fatal("Usage: <%s> <config file>" , sys.argv[0])
+        sys.exit(-1)
+
+    Config = ConfigParser.ConfigParser()
+    Config.read(sys.argv[1])
+
+    concurrency = Config.getint("testinfo","concurrency")
+    launch_script  = os.path.dirname (sys.argv[0]) + "/launch_spark_client.py"
+    while concurrency > 0:
+      name = "Thread_" + concurrency.__str__()
+      subprocess.Popen (["python", launch_script , name, sys.argv[1]])
+      concurrency = concurrency-1
+
+    logging.info ("Spark All Done.")
